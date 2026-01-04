816ab674d844c39edfc1ed3af1f069a16ac5d580
majusque <me@domain.com>
2026-01-04 21:47:32 +0000

    for local install

--- sampler.py
+++ sampler.py
@@ -6,14 +6,24 @@ pg.mixer.init()
 sample_1 = pg.mixer.Sound("/home/matt/samples/wimm/wi-piano-1.wav")
 sample_2 = pg.mixer.Sound("/home/matt/samples/wimm/wi-piano-2.wav")
 
-sequence  = []
-sequence.append(sample_1)
-sequence.append(sample_2)
+sequence_1  = []
+sequence_1.append(sample_1)
+sequence_1.append(sample_1)
+sequence_1.append(sample_2)
 
-t = 3.122
+rate = 3
+t = 0
+count = 0
 
 while True:
-    for sample in sequence:
-        pg.mixer.Channel(1).play(sample)
-        time.sleep(t)
+    
+    if t == 0:
+        pg.mixer.Channel(1).play(sequence_1[count])
+        count += 1
+    time.sleep(1)
+    t += 1
+    if t == rate:
+        t = 0
+    if count >= len(sequence_1):
+        count = 0
 
