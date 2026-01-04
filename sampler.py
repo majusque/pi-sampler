600603975e66333fe84a4d3c9a33ca7f552c9c1e
majusque <me@domain.com>
2026-01-04 16:45:07 +0000

    basic playback

--- sampler.py
+++ sampler.py
@@ -6,9 +6,14 @@ pg.mixer.init()
 sample_1 = pg.mixer.Sound("/home/matt/samples/wimm/wi-piano-1.wav")
 sample_2 = pg.mixer.Sound("/home/matt/samples/wimm/wi-piano-2.wav")
 
+sequence  = []
+sequence.append(sample_1)
+sequence.append(sample_2)
+
+t = 3.122
+
 while True:
-    pg.mixer.Channel(1).play(sample_1)
-    time.sleep(3.122)
-    pg.mixer.Channel(1).play(sample_2)
-    time.sleep(3.122)
+    for sample in sequence:
+        pg.mixer.Channel(1).play(sample)
+        time.sleep(t)
 
