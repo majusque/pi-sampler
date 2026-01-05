121ebd5ccb39432f46d51480d1339eb0634abf1b
Your Name <you@example.com>
2026-01-05 22:32:02 +0000

    sync in working

--- sampler.py
+++ sampler.py
@@ -1,10 +1,19 @@
 import pygame as pg
 import time
+import RPi.GPIO as GPIO
+
+def sync_trigger(Pin):
+    """
+    Function triggered by sync in voltage
+    """
+    global sync_count
+    sync_count += 1
+    print(sync_count)
 
 pg.mixer.init()
 
-sample_1 = pg.mixer.Sound("/home/matt/samples/wimm/wi-piano-1.wav")
-sample_2 = pg.mixer.Sound("/home/matt/samples/wimm/wi-piano-2.wav")
+sample_1 = pg.mixer.Sound("wi-piano-1.wav")
+sample_2 = pg.mixer.Sound("wi-piano-2.wav")
 
 sequence_1  = []
 sequence_1.append(sample_1)
@@ -14,6 +23,13 @@ sequence_1.append(sample_2)
 rate = 3
 t = 0
 count = 0
+
+sync_count = 0
+
+sync_in_pin = 2
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sync_in_pin, GPIO.IN)
+GPIO.add_event_detect(sync_in_pin, GPIO.FALLING, callback=sync_trigger)
 
 while True:
     
