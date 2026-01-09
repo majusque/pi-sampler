29b1813de9abf6651353862bd0a563cc727f4306
Your Name <you@example.com>
2026-01-09 19:04:20 +0000

    per sample delays

--- sampler.py
+++ sampler.py
@@ -22,13 +22,13 @@ def increment_seq_count(sync_count, rate, seq_count, sequence):
         seq_count += 1
         if seq_count >= len(sequence):
             seq_count = 0
-        
-            
     return seq_count
 
-def set_play(sync_count, rate, play):
+
+def set_play(sync_count, rate, play, sequence, seq_count):
+    delay = sequence[seq_count][1]
     play = False
-    if sync_count % rate == 0:
+    if (sync_count - delay) % rate == 0:
         play = True
     return play    
     
@@ -36,11 +36,12 @@ def set_play(sync_count, rate, play):
 def play_sample(play, channel, sequence, seq_count, vol):
     if play_all:        
         if play:
-            if sequence[seq_count] != "":
+            if sequence[seq_count][0] != "":
                 pg.mixer.Channel(channel).set_volume(vol)
-                pg.mixer.Channel(channel).play(sequence[seq_count])
+                pg.mixer.Channel(channel).play(sequence[seq_count][0])
             play = False
-        
+
+
 def button_callback(channel):
     global sync_count
     sync_count = 0
@@ -56,13 +57,13 @@ def button_callback(channel):
 #some initialisation
 pg.mixer.init()
 #display
-serial = i2c(port=1, address=0x3C)
-device = ssd1306(serial, rotate=0)
+# serial = i2c(port=1, address=0x3C)
+# device = ssd1306(serial, rotate=0)
 
-with canvas(device) as draw:
-    draw.text((10, 0), "SpAmpLyEr...", fill="white")
-    draw.text((10, 30), "EgJam Ind. 2025", fill="white")
-time.sleep(3)
+# with canvas(device) as draw:
+#     draw.text((10, 0), "SpAmpLyEr...", fill="white")
+#     draw.text((10, 30), "EgJam Ind. 2025", fill="white")
+# time.sleep(3)
 
 global sequences
 sequences = []
@@ -76,41 +77,44 @@ sample_5 = pg.mixer.Sound("drums.wav")
 sample_6 = pg.mixer.Sound("bass.wav")
 sample_7 = pg.mixer.Sound("hi-hats.wav")
 sample_8 = pg.mixer.Sound("ooh.wav")
+sample_9 = pg.mixer.Sound("wi-yeah.wav")
 
 sequence_1  = []
-sequence_1.append(sample_2)
-sequence_1.append(sample_1)
+sequence_1.append([sample_2, 0])
+sequence_1.append([sample_1, 0])
 sequences.append(sequence_1)
 
 sequence_2  = []
-sequence_2.append(sample_4)
-sequence_2.append(sample_3)
+sequence_2.append([sample_4, 0])
+sequence_2.append([sample_3, 0])
 #sequence_2.append("")
 sequences.append(sequence_2)
 
 sequence_3 = []
-sequence_3.append(sample_5)
+sequence_3.append([sample_5, 0])
 sequences.append(sequence_3)
 
 
 sequence_4 = []
-sequence_4.append(sample_6)
+sequence_4.append([sample_6, 0])
 sequences.append(sequence_4)
 
 
 sequence_5 = []
-sequence_5.append(sample_7)
+sequence_5.append([sample_7, 0])
 sequences.append(sequence_5)
 
 
 sequence_6 = []
-sequence_6.append(sample_8)
+sequence_6.append([sample_8, 6])
+sequence_6.append(["", 0])
+sequence_6.append([sample_9, 9])
 sequences.append(sequence_6)
 
 
 #choose an external sync signal (true) or use the internal one (false)
 sync_in = False
-internal_rate = 0.124
+internal_rate = 0.123
 
 sync_count = 0 #the current value of the sync trigger counter
 last_sync_count = 0
@@ -133,10 +137,11 @@ seq_counts = [0]*len(sequences)
 global play_seqs
 play_seqs = [True]*len(sequences)
 vols = [0.8,0.8,1.0,1.0,0.8,1.0]
-rates = [25,25,50,50,50,100] #length of each sequence to play
+rates = [25,25,50,50,50,50] #length of each sequence to play
 
 global play_all
 play_all = False
+play_refresh = int(1.0/0.123)
 
 while True: # main program loop
     
@@ -147,12 +152,14 @@ while True: # main program loop
         for i in range(0,len(sequences)):
             if play_seqs[i]:
                 seq_counts[i] = increment_seq_count(sync_count, rates[i], seq_counts[i], sequences[i]) #increment each sequences sample counter
-                plays[i] = set_play(sync_count, rates[i], plays[i]) # set whether to play the sequence at this loop step or not
+                plays[i] = set_play(sync_count, rates[i], plays[i], sequences[i], seq_counts[i]) # set whether to play the sequence at this loop step or not
                 play_sample(plays[i], i, sequences[i], seq_counts[i], vols[i]) # play the sample (or not) at this loop step
                 
 
     if sync_in == False:#internal clock
         sync_count += 1
         time.sleep(internal_rate)
+        
+
 
 
