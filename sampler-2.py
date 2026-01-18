beae422a18ba4a96ff0e17607b04da4138a53924
Your Name <you@example.com>
2026-01-18 21:48:19 +0000

    tft display instaed of oled

--- sampler-2.py
+++ sampler-2.py
@@ -1,7 +1,8 @@
 import pygame as pg
 import time
 import RPi.GPIO as GPIO
-
+from lib_tft144 import TFT144
+import spidev
 
 pg.mixer.init()
 
@@ -162,7 +163,20 @@ def rotary_count(dirstr):
         return 0
     
 def select_button_callback(channel):
-    print("select")
+    global select
+    select = True
+    
+def splash_load(TFT):
+    TFT.clear_display(TFT.BLACK)
+    TFT.put_string("SpAmpLer",28,28,TFT.WHITE,TFT.BLACK)  # std font 3 (default)
+    TFT.put_string("EgJam Ind. 2025", 24,80,TFT.WHITE, TFT.BLACK)     # doubled font 4
+    time.sleep(3)
+    TFT.clear_display(TFT.BLACK)
+    TFT.put_string("Menu",28,28,TFT.WHITE,TFT.BLACK)  # std font 3 (default)
+
+
+   
+
 
 #samples and sequences
 sample_paths = []
@@ -254,12 +268,30 @@ GPIO.add_event_detect(PIN_B,GPIO.RISING,callback=PIN_B_callback, bouncetime=100)
 GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(BUTTON,GPIO.RISING,callback=select_button_callback, bouncetime=500)
 
+
+#display
+RST = 18
+CE =   0    # 0 or 1 for CE0 / CE1 number (NOT the pin#)
+DC =  22    # Labeled on board as "A0"
+LED = 23    # LED backlight sinks 10-14 mA @ 3V
+#these pins defult value:
+#SCK = 11
+#MOSI (SDA) = 10 # Don't forget the other 2 SPI pins SCK and MOSI (SDA)
+TFT = TFT144(GPIO, spidev.SpiDev(), CE, DC, RST, LED, isRedBoard=False)
+TFT.clear_display(TFT.BLACK)
+splash_load(TFT)
+
+
+
+
 count = 0
 last_count = 0
 
 while True: # main program loop
     global dirstr
     dirstr = ""
+    global select 
+    select = False
     
     if sync_in == False:#internal clock
         sync()
@@ -272,5 +304,10 @@ while True: # main program loop
     if count != last_count:
         print(count)
         last_count = count
+    
+    if select:
+        print("select")
+        TFT.clear_display(TFT.BLACK)
+        TFT.put_string("select",10,10,TFT.WHITE,TFT.BLACK)  # std font 3 (default)
         
         
